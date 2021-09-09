e8c748adf608debea05d0e5b37941097d657b9e7
Seeun <duck_sim@naver.com>
2021-09-10 02:23:37 +0900

    practe1 파일 커밋

---
+++ practice1.py
@@ -0,0 +1,9 @@
+# 사용자로부터 숫자를 입력받아 그 숫자의 크기 만큼 "*"출력하기
+
+number_input = input("숫자를 입력해주세요 : ")
+
+user_input_number = int(number_input)
+
+for i in range(1,user_input_number+1):
+    print(i*"*")
+    print("와우 정말 대단한걸~! 따봉!")
